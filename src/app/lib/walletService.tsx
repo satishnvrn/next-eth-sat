c404c9837838135c673674de37bf6ee0ec65f47b
satishnvrn <nvrn.satish@gmail.com>
2023-07-08 01:28:40 +0530

    Added fund and withdraw ability

--- src/app/lib/walletService.tsx
+++ src/app/lib/walletService.tsx
@@ -1,7 +1,13 @@
-import { ethers } from "ethers";
+import {
+  ContractTransactionReceipt,
+  ContractTransactionResponse,
+  Provider,
+  ethers,
+} from 'ethers';
+import { abi, contractAddress } from './constants';
 
 export const findEthereumExists = () => {
-  if (window && window.ethereum !== "undefined") {
+  if (window && window.ethereum !== 'undefined') {
     return true;
   }
 
@@ -10,8 +16,8 @@ export const findEthereumExists = () => {
 
 export const connectFundMe = async () => {
   try {
-    await ethereum.request({ method: "eth_requestAccounts" });
-    const accounts = await ethereum.request({ method: "eth_accounts" })
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
     return accounts;
   } catch (error) {
     console.log('error', error);
@@ -27,4 +33,54 @@ export const getBalance = async (address: string) => {
     console.log('error', error);
     return '';
   }
-}
+};
+
+export const withdraw = async () => {
+  try {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const transactionResponse = await contract.withdraw();
+    await listenForTransactionMine(transactionResponse, provider);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+export const fund = async (ethAmount) => {
+  try {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const transactionResponse = await contract.fund({
+      value: ethers.parseEther(ethAmount),
+    });
+    await listenForTransactionMine(transactionResponse, provider);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+export const listenForTransactionMine = async (
+  transactionResponse: ContractTransactionResponse,
+  provider: Provider,
+) => {
+  return new Promise((resolve, reject) => {
+    try {
+      provider.once(
+        transactionResponse.hash,
+        (transactionReceipt: ContractTransactionReceipt) => {
+          console.log(
+            `Completed with ${transactionReceipt.confirmations} confirmations. `,
+          );
+          resolve(true);
+        },
+      );
+    } catch (error) {
+      console.log('listenForTransactionMine error', error);
+      reject(error);
+    }
+  });
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react';
 import {
   connectFundMe,
   findEthereumExists,
+  fund,
   getBalance,
+  withdraw,
 } from './lib/walletService';
 import Button from './components/button';
 
@@ -12,15 +14,23 @@ export default function Home() {
   const [ethereumExists, setEthereumExists] = useState('loading');
   const [connectedAccounts, setConnectedAccounts] = useState([]);
   const [balanceETH, setBalanceETH] = useState('');
+  const [ethAmount, setEthAmount] = useState('0');
 
   const handleGetBalance = async () => {
     const balance = await getBalance(connectedAccounts[0]);
     setBalanceETH(balance);
   };
 
+  const handleFundMe = async () => {
+    await fund(ethAmount);
+  };
+
+  const handleWithdraw = async () => {
+    await withdraw();
+  };
+
   useEffect(() => {
     const isWalletExists = findEthereumExists();
-    console.log('isWalletExists', isWalletExists);
     if (isWalletExists) {
       setEthereumExists('exists');
     } else {
@@ -30,41 +40,51 @@ export default function Home() {
   return (
     <div>
       {ethereumExists === 'exists' && (
-        <div className="flex flex-row justify-around">
-          <div>
-            <Button
-              onClick={async () => {
-                const accounts = await connectFundMe();
-                setConnectedAccounts(accounts);
-              }}
-              disabled={connectedAccounts.length > 0}
-            >
-              {connectedAccounts.length > 0
-                ? 'Connected'
-                : 'Connect Fund Me Contract'}
-            </Button>
-            <div className="mt-2.5">
-              {connectedAccounts.map((acc) => (
-                <div
-                  className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full"
-                  key={acc}
-                >
-                  {acc}
-                </div>
-              ))}
+        <div>
+          <div className="flex flex-row justify-around">
+            <div>
+              <Button
+                onClick={async () => {
+                  const accounts = await connectFundMe();
+                  setConnectedAccounts(accounts);
+                }}
+                disabled={connectedAccounts.length > 0}
+              >
+                {connectedAccounts.length > 0
+                  ? 'Connected'
+                  : 'Connect Fund Me Contract'}
+              </Button>
+              <div className="mt-2.5">
+                {connectedAccounts.map((acc) => (
+                  <div
+                    className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full"
+                    key={acc}
+                  >
+                    {acc}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div>
-            {connectedAccounts.length > 0 && (
-              <div className="mt-5">
-                <Button onClick={handleGetBalance}>Get Balance</Button>
-                <div>
-                  <div className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full">
-                    {balanceETH}
+            <div>
+              {connectedAccounts.length > 0 && (
+                <div className="mt-5">
+                  <Button onClick={handleGetBalance}>Get Balance</Button>
+                  <div>
+                    <div className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full">
+                      {balanceETH}
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
+          </div>
+          <div className='flex'>
+            <label>ETH Amount</label>
+            <input type='number' id='ethAmount' placeholder='0.1' value={ethAmount} onChange={(event) => {
+              setEthAmount(event.target.value);
+            }}/>
+            <Button onClick={handleFundMe}>Fund</Button>
+            <Button onClick={handleWithdraw}>Withdraw</Button>
           </div>
         </div>
       )}
